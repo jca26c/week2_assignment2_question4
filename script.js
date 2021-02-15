1450e54f6ab502f4677d904c93708d1774b7805d
jca26c <78522594+jca26c@users.noreply.github.com>
2021-02-15 13:43:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,47 @@
- require([
-        "esri/Map",
-        "esri/views/MapView",
-        "esri/layers/ImageryLayer",
-        "esri/layers/support/RasterFunction"
-      ], function(Map, MapView, ImageryLayer, RasterFunction) {
-        /***************************************
-         * Set up popup template of image layer
-         **************************************/
-
-        var imagePopupTemplate = {
-          // autocasts as new PopupTemplate()
-          title: "Data from {SensorName} satellite",
-          content:
-            "Rendered RGB values: <b>{Raster.ServicePixelValue} </b>" +
-            "<br>Original values (B, G, R, NIR): <b>{Raster.ItemPixelValue} </b>"
-        };
-
-        /*******************************************************************
-         * Create image layer with server defined raster function templates
-         ******************************************************************/
-
-        var serviceRFT = new RasterFunction({
-          functionName: "NDVI",
-          "rasterFunctionArguments": {
-    "VisibleBandID": 2,
-    "InfraredBandID": 1
+require([
+ "esri/Map",
+ "esri/views/MapView",
+ "esri/layers/ImageryLayer",
+ "esri/layers/support/RasterFunction"
+], function(Map, MapView, ImageryLayer, RasterFunction) {
+ var imagePopupTemplate = {
+  title: "Data from {SensorName} satellite",
+  content:
+  "Rendered RGB values: <b>{Raster.ServicePixelValue} </b>" +
+  "<br>Original values (B, G, R, NIR): <b>{Raster.ItemPixelValue} </b>"
+ };
+ 
+ var serviceRFT = new RasterFunction({
+  functionName: "NDVI",
+  "rasterFunctionArguments": {
+   "VisibleBandID": 2,
+   "InfraredBandID": 1
   },
-          variableName: "Raster"
-        });
-
-        var layer = new ImageryLayer({
-          url: "https://landsat2.arcgis.com/arcgis/rest/services/Landsat8_Views/ImageServer",
-          renderingRule: serviceRFT,
-          popupTemplate: imagePopupTemplate
-        });
-
-        /*************************
-         * Add image layer to map
-         ************************/
-
-        var map = new Map({
-          basemap: "hybrid",
-          layers: [layer]
-        });
-
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          center: {
-            // autocasts as esri/geometry/Point
-            x: -90.23,
-            y: 38.61,
-            spatialReference: 4326
-          },
-          zoom: 7,
-          popup: {
-            actions: []
-          }
-        });
-      });
+  variableName: "Raster"
+ });
+ 
+ var layer = new ImageryLayer({
+  url: "https://landsat2.arcgis.com/arcgis/rest/services/Landsat8_Views/ImageServer",
+  renderingRule: serviceRFT,
+  popupTemplate: imagePopupTemplate
+ });
+ 
+ var map = new Map({
+  basemap: "hybrid",
+  layers: [layer]
+ });
+ 
+ var view = new MapView({
+  container: "viewDiv",
+  map: map,
+  center:{ 
+  x: -90.23,
+  y: 38.61,
+  spatialReference: 4326
+ },
+  zoom: 7,
+  popup: {
+   actions: []
+  }
+ });
+});
